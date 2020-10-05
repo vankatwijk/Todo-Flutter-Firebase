704d19b260997ca95ac6d199076098aa7b60d81d
vankatwijk <hendrikus.hpvk@gmail.com>
2020-10-05 17:33:31 +0200

    issue close the dialog

--- Todo/lib/main.dart
+++ Todo/lib/main.dart
@@ -54,6 +54,7 @@ class _MyAppState extends State<MyApp> {
                     setState((){
                       todos.add(input);
                     });
+                    Navigator.of(context).pop(); // closes the dialog
                   },child:Text("Add"))
                 ]
               );
