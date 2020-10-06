44821e7c0ab1a7ff13ef6056fcb47c626c1d4835
vankatwijk <hendrikus.hpvk@gmail.com>
2020-10-06 13:30:54 +0200

    init the firestore package

--- Todo/lib/main.dart
+++ Todo/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() =>
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MaterialApp(
     debugShowCheckedModeBanner:false,
     theme: ThemeData(
@@ -11,6 +14,7 @@ void main() =>
     ),
     home:MyApp(),
   ));
+}
 
 //snippet stfl for vscode
 class MyApp extends StatefulWidget {
@@ -22,15 +26,33 @@ class _MyAppState extends State<MyApp> {
   List todos = List();
   String input = "";
 
-  @override
-  void initState() { 
-    super.initState();
-    todos.add("Item1");
-    todos.add("Item2");
-    todos.add("Item3");
-    todos.add("Item4");
-    
+  createTodos(){
+    DocumentReference documentReference = FirebaseFirestore.instance.collection("MyTodos").doc(input);
+
+    //Map
+    Map<String, String> todos = {
+      "todoTitle":input
+    };
+    documentReference.set(todos).whenComplete((){
+      print("$input created");
+    });
   }
+
+  deleteTodos(){
+
+  }
+
+
+
+  // @override
+  // void initState() { 
+  //   super.initState();
+  //   todos.add("Item1");
+  //   todos.add("Item2");
+  //   todos.add("Item3");
+  //   todos.add("Item4");
+    
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -54,9 +76,10 @@ class _MyAppState extends State<MyApp> {
                 ),
                 actions: <Widget>[
                   FlatButton(onPressed: (){
-                    setState((){
-                      todos.add(input);
-                    });
+                    // setState((){
+                    //   todos.add(input);
+                    // });
+                    createTodos();
                     Navigator.of(context).pop(); // closes the dialog
                   },child:Text("Add"))
                 ]
@@ -70,18 +93,42 @@ class _MyAppState extends State<MyApp> {
         )
 
       ),
-      body: ListView.builder(
-        itemCount: todos.length,
-        itemBuilder: (BuildContext context, int index){
+      // body: ListView.builder(
+      //   itemCount: todos.length,
+      //   itemBuilder: (BuildContext context, int index){
+      //     return Dismissible(
+      //       key:Key(todos[index]), 
+      //       child:Card(
+      //         elevation: 4,
+      //         margin: EdgeInsets.all(8),
+      //         shape:RoundedRectangleBorder(
+      //           borderRadius: BorderRadius.circular(16)),
+      //         child: ListTile(
+      //           title:Text(todos[index]),
+      //           trailing: IconButton(icon: Icon(Icons.delete, color:Colors.red),
+      //           onPressed:(){
+      //             setState(() {
+      //               todos.removeAt(index);
+      //             });
+      //           }),
+      //         ),
+      //       ));
+      //   }),
+      body: StreamBuilder(stream:FirebaseFirestore.instance.collection("MyTodos").snapshots(),builder: (context,snapshots){
+        return ListView.builder(
+        shrinkWrap:true,
+        itemCount: snapshots.data.documents.length,
+        itemBuilder: (context,index){
+          DocumentSnapshot documentSnapshot = snapshots.data.documents[index];
           return Dismissible(
-            key:Key(todos[index]), 
+            key:Key(index.toString()), 
             child:Card(
               elevation: 4,
               margin: EdgeInsets.all(8),
               shape:RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(16)),
               child: ListTile(
-                title:Text(todos[index]),
+                title:Text(documentSnapshot["todoTitle"]),
                 trailing: IconButton(icon: Icon(Icons.delete, color:Colors.red),
                 onPressed:(){
                   setState(() {
@@ -90,7 +137,8 @@ class _MyAppState extends State<MyApp> {
                 }),
               ),
             ));
-        }),
+        });
+      }),
     );
   }
 }
